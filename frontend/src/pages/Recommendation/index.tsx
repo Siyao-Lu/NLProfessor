377e42d9ca17d56a0cabe9d399027c77f3998a77
Mingye Chen <mingyechen82@gmail.com>
2022-11-24 22:55:21 -0500

    Update css
    Also add examples.

--- frontend/src/pages/Recommendation/index.tsx
+++ frontend/src/pages/Recommendation/index.tsx
@@ -2,38 +2,50 @@ import React from "react";
 import Recommendation from "./recommendation";
 
 const RecommendationList = () => {
-    return (
-        <div>
-            <Recommendation
-                courses={[
-                    {
-                        basedOn: ["machine learning", "team"],
-                        courseName: "Conversational AI",
-                        courseDept: "EECS",
-                        courseNum: 449,
-                    },
-                    {
-                        basedOn: ["machine learning", "team"],
-                        courseName: "Conversational AI",
-                        courseDept: "EECS",
-                        courseNum: 449,
-                    },
-                    {
-                        basedOn: ["machine learning", "team"],
-                        courseName: "Conversational AI",
-                        courseDept: "EECS",
-                        courseNum: 449,
-                    },
-                    {
-                        basedOn: ["machine learning", "team"],
-                        courseName: "Conversational AI",
-                        courseDept: "EECS",
-                        courseNum: 449,
-                    },
-                ]}
-                basedOn={["junior", "machine learning", "team"]}
-            />
-        </div>
-    );
+  return (
+    <div>
+      <Recommendation
+        courses={[
+          {
+            basedOn: ["machine learning", "team"],
+            courseName: "Conversational AI",
+            courseDept: "EECS",
+            courseNum: 449,
+          },
+          {
+            basedOn: ["junior", "team"],
+            courseName: "Web Systems",
+            courseDept: "EECS",
+            courseNum: 485,
+          },
+          {
+            basedOn: ["machine learning"],
+            courseName: "Intro Natural Language processing",
+            courseDept: "EECS",
+            courseNum: 487,
+          },
+          {
+            basedOn: ["junior"],
+            courseName: "Intro Algorithms",
+            courseDept: "EECS",
+            courseNum: 477,
+          },
+          {
+            basedOn: ["machine learning", "team"],
+            courseName: "Conversational AI",
+            courseDept: "EECS",
+            courseNum: 449,
+          },
+          {
+            basedOn: ["machine learning", "team"],
+            courseName: "Conversational AI",
+            courseDept: "EECS",
+            courseNum: 449,
+          },
+        ]}
+        basedOn={["junior", "machine learning", "team"]}
+      />
+    </div>
+  );
 };
 export default RecommendationList;
