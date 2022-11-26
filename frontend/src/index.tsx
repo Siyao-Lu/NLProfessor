7688a4efda35974fdb75de3b87f1d879deb6d624
Mingye Chen <mingyechen82@gmail.com>
2022-11-25 19:23:47 -0500

    Change style, fix lint

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,9 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-import RecommendationList from "./pages/Recommendation";
-import Front from "./pages/Front";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
 import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.render(
